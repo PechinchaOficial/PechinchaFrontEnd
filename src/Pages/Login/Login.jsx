ebc3631468c5be79abd10bba5ef8767eb7470db4
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-05-26 23:32:11 -0300

    iniciei a estrutura

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,12 +5,39 @@ function Login() {
         <main className='conteudo'>
             <h1>Login</h1>
             <section>
+                    <div  className='forms-register'>
+                        <h2>Entre com a sua conta</h2>
+                        <h3>Preencha todos os campos</h3>
 
-                    <div className='teste'>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quae magni consequatur accusamus blanditiis voluptatibus asperiores esse maiores possimus vero labore, illum provident hic obcaecati praesentium eligendi. Ullam, voluptatibus quisquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur non voluptas, ad hic nulla aut eius nihil laudantium fugiat ipsum mollitia minima optio nemo quas architecto quaerat quos aliquam molestiae.</p>
+                            <form action="" className='forms-login'>
+                                <label htmlFor="">Email</label>
+                                <input type="text"  placeholder='Digite seu Email'/>
+
+                                <label htmlFor="">Senha</label>
+                                <input type="text" placeholder='Digite sua senha' />
+
+                                <label htmlFor="">Confirmar sua senha</label>
+                                <input type="text" placeholder='Digite sua senha' />
+
+                                <p>Esqueceu sua senha? <a href="#">Recupere aqui</a></p>
+
+                                <button>ENTRAR</button>
+
+                            </form>
+
+                            <p>Ao continuar com o acesso você concorda com a nossa <a href="#">politica de privacidade</a></p>
 
                     </div>
-                    
+                    <div className='banner-login'>
+                            <h2>
+                                Bem vindo de volta! 
+                            </h2>
+                            <p>Clique abaixo caso ainda não tenha uma conta e aproveite de todas as funções</p>
+
+                            <button>
+                                    CADASTRAR
+                            </button>
+                    </div>
             </section>
         </main>
     )
