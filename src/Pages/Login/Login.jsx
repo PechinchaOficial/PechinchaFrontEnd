e156429d9a9d83bc5de2eb338c70a9bd3a53e598
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-05-26 22:04:08 -0300

    primeira alteração

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,16 @@
-import styles from './Login.module.css'
+import  './Login.css';
 
 function Login() {
     return (
         <main className='conteudo'>
             <h1>Login</h1>
             <section>
-                
+
+                    <div className='teste'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quae magni consequatur accusamus blanditiis voluptatibus asperiores esse maiores possimus vero labore, illum provident hic obcaecati praesentium eligendi. Ullam, voluptatibus quisquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur non voluptas, ad hic nulla aut eius nihil laudantium fugiat ipsum mollitia minima optio nemo quas architecto quaerat quos aliquam molestiae.</p>
+
+                    </div>
+                    
             </section>
         </main>
     )
