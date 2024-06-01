14bb65b1f6dbc25882568939d225a0223f7cec88
Jessica Magalhães da silva <112392689+Jesmgl@users.noreply.github.com>
2024-06-01 11:47:05 -0300

    Udpating about us

--- src/ConsumerPages/AboutUs/MissionSection/Mission.jsx
+++ src/ConsumerPages/AboutUs/MissionSection/Mission.jsx
@@ -4,14 +4,14 @@ import missao from '../../../assets/img/nossa.missao.png'
 function Mission () {
     return(
 
-        <section  id='conteudo'>
-            <div className={Styles.missao}>
+        <section className={Styles.missao}  id='container'>
+           
                 <img className={Styles.imagem_missao} src={missao}/>
                 <div className={Styles.texto_missao}>
                     <div className={Styles.titulo_missao}></div>
                 </div>
                 
-            </div>
+           
         </section>
 
     )
