9b219243526c308dbbbf69d306cc6b3ea49698d7
Jessica Magalhães da silva <112392689+Jesmgl@users.noreply.github.com>
2024-05-31 22:37:58 -0300

    Alteration in paste About Us

--- src/ConsumerPages/AboutUs/WhoSection/Who.jsx
+++ src/ConsumerPages/AboutUs/WhoSection/Who.jsx
@@ -3,11 +3,11 @@ import Styles from './Who.module.css'
 function Who(){
     return (
 
-        <section className={Styles.container} id='container'>
+        <section className={Styles.container} id='conteudo'>
             <div className={Styles.block_red}>
                 <h1 className={Styles.red}>Quem Somos</h1>
             </div>
-            <p>Nosso objetivo é unir forças contra o desperdício de alimentos! nascemos para revolucionar a forma como consumimos e a nossa missão é ajudar</p>
+            <p className={Styles.text}>Nosso objetivo é unir forças contra o desperdício de alimentos! nascemos para revolucionar a forma como consumimos e a nossa missão é ajudar</p>
         </section>
 
     )
