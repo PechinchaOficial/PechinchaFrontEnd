5a69d79f5fd295d6e87e1cd706282fc3dd4985c2
GabrielGallo1 <gabriel.gallo194@gmail.com>
2024-06-03 22:46:17 -0300

    Conserto da hero da HomeMarket

--- src/MarketPages/Home/HeroSection/HeroSection.jsx
+++ src/MarketPages/Home/HeroSection/HeroSection.jsx
@@ -1,11 +1,12 @@
 import Styles from './HeroSection.module.css'
+import Bannerz from '../../../assets/img/HeroMercado1.svg'
 
 function HeroMarket() {
     return(
         <section className={Styles.Background} id='container'>
             <article className={Styles.Hero}>
                 <div className={Styles.Banner}>
-
+                    <img src={Bannerz} alt=""  className={Styles.Bannerz}/>
                 </div>
             </article>
             <article className={Styles.BodySect}>
