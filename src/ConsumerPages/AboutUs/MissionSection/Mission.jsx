78690ee3b8f60405491a282a8f8014cba9aeb468
Jessica Magalhães da silva <112392689+Jesmgl@users.noreply.github.com>
2024-06-05 22:43:07 -0300

    Insert photos in About Us

--- src/ConsumerPages/AboutUs/MissionSection/Mission.jsx
+++ src/ConsumerPages/AboutUs/MissionSection/Mission.jsx
@@ -8,7 +8,10 @@ function Mission () {
            
                 <img className={Styles.imagem_missao} src={missao}/>
                 <div className={Styles.texto_missao}>
-                    <div className={Styles.titulo_missao}></div>
+                    <div className={Styles.titulo_missao}>
+                        <h1 className={Styles.mission}>NOSSA MISSÃO</h1>
+                    </div>
+                    <p className={Styles.text}> Nosso intuito é combater o <span className={Styles.word}> desperdício </span> e a fome. criamos o Pechincha para resolver esse problema e trazer comida para a mesa para as famílias de bem.</p>
                 </div>
                 
            
