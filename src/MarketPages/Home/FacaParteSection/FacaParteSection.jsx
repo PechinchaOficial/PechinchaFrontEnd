59c585cef705bce8251f20c00d3e5fe5f27cf2dd
GabrielGallo1 <gabriel.gallo194@gmail.com>
2024-06-06 21:08:19 -0300

    update main market page

--- src/MarketPages/Home/FacaParteSection/FacaParteSection.jsx
+++ src/MarketPages/Home/FacaParteSection/FacaParteSection.jsx
@@ -1,18 +1,23 @@
 import Styles from "./FacaParte.module.css";
+import Triade from '../../../assets/img/Triade.jpeg'
+import Pechincha from '../../../assets/img/Pechincha.jpeg'
+import Pechincha1 from '../../../assets/img/Pechincha1.jpeg'
+import Pechincha2 from '../../../assets/img/Pechincha2.jpeg'
+
 
 function FacaParte() {
     return (
         <section className={Styles.Background} id="container">
             <article className={Styles.BoxLeft}>
                 <h1 className={Styles.Title}>FAÇA PARTE DESSA EQUIPE</h1>
-                <p className={Styles.Text}>nós somos o app nº1 do Brasil contra o desperdício de alimentos!nascemos para revolucionar a forma como consumimos e a nossa missão.</p>
+                <p className={Styles.Text}>nós somos o website nº1 do Brasil contra o desperdício de alimentos! Nascemos para revolucionar a forma como consumimos e a nossa missão.</p>
                 <button className={Styles.Btn}>Saiba mais</button>
             </article>
             <article className={Styles.BoxRight}>
-                 <div className={Styles.Box} id={Styles.Box1}></div> {/* Caixas de fotos */}
-                <div className={Styles.Box} id={Styles.Box2}></div>
-                <div className={Styles.Box} id={Styles.Box3}></div>
-                <div className={Styles.Box} id={Styles.Box4}></div>
+                 <div className={Styles.Box} id={Styles.Box1}><img className={Styles.img} height={300} width={260} src={Pechincha} alt="P.O do Pechincha, Scrum Master do Pechincha e o Fincanceiro do Pechincha" /></div>
+                <div className={Styles.Box} id={Styles.Box2}><img className={Styles.img} height={400} width={240} src={Triade} alt="Todo o esquadrão do Pechincha" /></div>
+                <div className={Styles.Box} id={Styles.Box3}><img className={Styles.img} height={400} width={260} src={Pechincha1} alt="Todo o esquadrão do Pechincha" /></div>
+                <div className={Styles.Box} id={Styles.Box4}><img className={Styles.img} height={250} width={260} src={Pechincha2} alt="Todo o esquadrão do Pechincha" /></div>
             </article>
         </section>
     )
