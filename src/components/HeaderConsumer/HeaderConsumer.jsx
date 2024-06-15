b0ce3431aebd3b5eba1cd2fdd69cf65202216178
Ka1quee <kaiqueoliveira375@gmail.com>
2024-06-14 22:05:33 -0300

    Function show options

--- src/components/HeaderConsumer/HeaderConsumer.jsx
+++ src/components/HeaderConsumer/HeaderConsumer.jsx
@@ -5,16 +5,25 @@ import Button from '../Button/Button'
 import SearchBar from '../SearchBar/SearchBar';
 import BtnLang from '../BtnLang/BtnLang';
 import user from '../../assets/img/user.png'
+import { useTranslation } from "react-i18next";
+import React from 'react';
 
 function HeaderConsumer() {
+    const { t } = useTranslation();
 
+    const profile_option = React.useRef();
+
+    function ShowOption(){
+        profile_option.current.style.height='500px'
+    }
     const scrollToTop = () => {
         window.scrollTo({
-          top: 0,
-          behavior: "auto"
+            top: 0,
+            behavior: "auto"
         });
-      };
+    };
 
+    
 
     return (
         <header className={styles.cabecalho}>
@@ -27,14 +36,14 @@ function HeaderConsumer() {
 
 
                         <li>
-                        <Link onClick={scrollToTop}to="/about">Sobre nós</Link>
+                            <Link onClick={scrollToTop} to="/about">Sobre nós</Link>
                         </li>
 
                         <li>
-                        <Link onClick={scrollToTop} to="/homemarket">Seja Parceiro</Link>
+                            <Link onClick={scrollToTop} to="/homemarket">Seja Parceiro</Link>
                         </li>
 
- 
+
                     </ul>
                 </div>
             </div>
@@ -42,18 +51,13 @@ function HeaderConsumer() {
 
                 <div className={styles.container_header} id='container'>
                     <div className={styles.logo}>
-                    <Link onClick={scrollToTop} to="/">
+                        <Link onClick={scrollToTop} to="/">
                             <img src={logo} alt="Logo" className={styles.img_logo} />
                         </Link>
                     </div>
 
                     <SearchBar />
                     <div className={styles.nav_right}>
-                    <Link onClick={scrollToTop}to='/userprofile'>
-                            <div className={styles.user_icon}>
-                               <img src={user} alt="" className={styles.user} />
-                            </div>
-                        </Link>
 
 
                         <Link onClick={scrollToTop} to='/cart'>
@@ -62,12 +66,27 @@ function HeaderConsumer() {
                                     <span>1</span>
                                 </div>
                                 <i class="fa-solid fa-cart-shopping" id={styles.cart_icon}></i>
-                                <nav className={styles.nav_profile}>
 
-</nav>
                             </div>
 
                         </Link>
+
+                        <Link onClick={scrollToTop}  to='/userprofile' >
+                            <div className={styles.user_icon}>
+                                <img src={user} alt="" className={styles.user} />
+                            </div>
+                        </Link>
+
+
+                        <nav className={styles.nav_profile} ref={profile_option}>
+                            <ul>
+                                <li><i class="fa-solid fa-pen"></i> <a href="#">{t("Editar informações")}</a></li>
+                                <li><i class="fa-solid fa-shield-halved"></i> <a href="#">{t("Segurança")}</a></li>
+                                <li><i class="fa-solid fa-circle-question"></i><a href="#">{t("Ajuda")}</a></li>
+                                <li><i class="fa-solid fa-right-from-bracket"></i><Link to="/homedeslogado">{t("Sair")}</Link></li>
+
+                            </ul>
+                        </nav>
                     </div>
 
                 </div>
@@ -79,35 +98,35 @@ function HeaderConsumer() {
                 <div className={styles.container_header} id='container'>
                     <ul className={styles.nav_link}>
                         <li>
-                        <Link onClick={scrollToTop} className={styles.principal_link} to="/listagem">PARA VOCÊ</Link>
+                            <Link onClick={scrollToTop} className={styles.principal_link} to="/listagem">PARA VOCÊ</Link>
                         </li>
 
 
                         <li>
-                        <Link onClick={scrollToTop}to="/listagem">Categorias</Link>
+                            <Link onClick={scrollToTop} to="/listagem">Categorias</Link>
                         </li>
 
                         <li>
-                        <Link onClick={scrollToTop} to="/snacks">Snacks</Link>
+                            <Link onClick={scrollToTop} to="/snacks">Snacks</Link>
                         </li>
 
                         <li>
-                        <Link onClick={scrollToTop} to="/bebidas">Bebibas</Link>
+                            <Link onClick={scrollToTop} to="/bebidas">Bebibas</Link>
                         </li>
 
 
                         <li>
-                        <Link onClick={scrollToTop}to="/graos">Grãos</Link>
+                            <Link onClick={scrollToTop} to="/graos">Grãos</Link>
                         </li>
 
                         <li>
-                        <Link onClick={scrollToTop} to="/massas">Massas</Link>
+                            <Link onClick={scrollToTop} to="/massas">Massas</Link>
                         </li>
 
                         <li>
-                        <Link onClick={scrollToTop} to="/listagem">Variados</Link>
+                            <Link onClick={scrollToTop} to="/listagem">Variados</Link>
                         </li>
-                        
+
 
                     </ul>
                 </div>
