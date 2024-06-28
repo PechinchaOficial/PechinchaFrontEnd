e0ee052626a6ff5fa5abb70755fc4e10ca348850
viagem <jpina7989@gmail.com>
2024-06-28 00:34:05 -0300

    adding product

--- src/assets/data/localData.js
+++ src/assets/data/localData.js
@@ -1,53 +1,309 @@
 
 const LocalData = [
+
+    //---------- Produtos Grãos -----------///
     {
         id: 1,
-        fotoproduto: '',
-        nome: 'Feijão Bom grão',
-        datavalidade: '12/10/2024',
-        preco: '1,90',
-        desconto: '50%',
+        fotoproduto: 'https://meufestval.vtexassets.com/arquivos/ids/182635/Feijao-Caldo-Bom-Carioca-1KG.png?v=637729395609600000',
+        nome: 'Feijão Bom      grão',
+        datavalidade: '03/09/2024',
+        preco: '6,78',
+        desconto: '15%',
         qtdUnit: '20'
     },
 
     {
         id: 2,
-        fotoproduto: ``,
-        nome: 'Suco de maça Ades Bebida',
-        preco: '9,90',
-        datavalidade: '12/10/2024',
-        desconto: '50%',
+        fotoproduto: 'https://www.solito.com.br/site/wp-content/uploads/2017/06/mockup_site_feijao-carioca-2-450x649.png',
+        nome: 'Feijão Solito     grão',
+        datavalidade: '02/11/2024',
+        preco: '4,49',
+        desconto: '25%',
         qtdUnit: '25'
     },
 
     {
         id: 3,
-        fotoproduto: ``,
-        nome: 'Salgadinho cheetos snack',
-        preco: '9,90',
-        datavalidade: '12/10/2024',
-        desconto: '50%',
-        qtdUnit: '25'
+        fotoproduto: 'https://obahortifruti.vtexassets.com/arquivos/ids/4250495/Feijao-Carioca-Camil-1kg.jpg?v=638144112468500000',
+        nome: 'Feijão Camil    grão',
+        datavalidade: '12/09/2024',
+        preco: '6,38',
+        desconto: '15%',
+        qtdUnit: '27'
     },
 
     {
         id: 4,
-        fotoproduto: ``,
-        nome: 'Macarrão massa',
-        preco: '9,90',
+        fotoproduto: 'https://propao.agilecdn.com.br/4491_1.jpg',
+        nome: 'Feijão Kicaldo    grão',
         datavalidade: '12/10/2024',
+        preco: '10,12',
+        desconto: '15%',
+        qtdUnit: '20'
+    },
+
+    {
+        id: 5,
+        fotoproduto: 'https://io.convertiez.com.br/m/trimais/shop/products/images/886/medium/feijao-camil-preto-1kg_881.jpg',
+        nome: 'Feijão Camil    grão',
+        datavalidade: '11/09/2024',
+        preco: '7,12',
+        desconto: '25%',
+        qtdUnit: '23'
+    },
+
+    {
+        id: 6,
+        fotoproduto: 'https://destro.fbitsstatic.net/img/p/feijao-preto-kicaldo-tipo-1-1kg-74804/261354.jpg?w=500&h=500&v=no-change&qs=ignore',
+        nome: 'Feijão Kicaldo    grão',
+        datavalidade: '01/10/2024',
+        preco: '4,89',
+        desconto: '30%',
+        qtdUnit: '15'
+    },
+
+    //--------- Produtos Bebidas ----------//
+    {
+        id: 7,
+        fotoproduto: `https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14021/medium/bebida-a-base-de-soja-ades-maca-1l_46941.png`,
+        nome: 'Suco de Maça Ades Bebida',
+        preco: '6,23',
+        datavalidade: '10/08/2024',
+        desconto: '20%',
+        qtdUnit: '25'
+    },
+
+    {
+        id: 8,
+        fotoproduto: `https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14019/medium/bebida-a-base-de-soja-ades-uva-1l_10513.png`,
+        nome: 'Suco de Uva Ades Bebida',
+        preco: '5,06',
+        datavalidade: '24/08/2024',
+        desconto: '35%',
+        qtdUnit: '14'
+    },
+
+    {
+        id: 9,
+        fotoproduto: `https://andinacocacola.vtexassets.com/arquivos/ids/157773/110462_COCA-%E2%80%93-AdesLaranja1L.jpg?v=638411177589670000`,
+        nome: 'Suco de Laranja Ades Bebida',
+        preco: '6,23',
+        datavalidade: '27/10/2024',
+        desconto: '20%',
+        qtdUnit: '20'
+    },
+
+    {
+        id: 10,
+        fotoproduto: `https://solaranja.loji.com.br/storage/uploads/jrpatVWzvbEkN97K7vZY06mT0pkLwr5d1rhhreui.png`,
+        nome: 'Suco de Soja Ades Bebida',
+        preco: '7,64',
+        datavalidade: '12/09/2024',
+        desconto: '15%',
+        qtdUnit: '10'
+    },
+
+    {
+        id: 11,
+        fotoproduto: `https://www.imigrantesbebidas.com.br/bebida/images/products/full/4585-nectar-de-maracuja-del-valle-1l.jpg`,
+        nome: 'Suco Del Valle Bebida',
+        preco: '2,64',
+        datavalidade: '10/07/2024',
+        desconto: '50%',
+        qtdUnit: '09'
+    },
+
+    {
+        id: 12,
+        fotoproduto: `https://static.clubeextra.com.br/img/uploads/1/7/25295007.png`,
+        nome: 'Suco Del Valle Bebida',
+        preco: '2,64',
+        datavalidade: '10/07/2024',
+        desconto: '50%',
+        qtdUnit: '11'
+    },
+
+    //----------- Produtos Snack ----------//
+    {
+        id: 13,
+        fotoproduto: `https://docemalu.vtexassets.com/arquivos/ids/5339948-800-auto?v=638421720616770000&width=800&height=auto&aspect=true`,
+        nome: 'Salgadinho Cheetos snack',
+        preco: '4,30',
+        datavalidade: '08/07/2024',
+        desconto: '50%',
+        qtdUnit: '22'
+    },
+
+    {
+        id: 14,
+        fotoproduto: `https://img.irroba.com.br/fit-in/600x600/filters:fill(transparent):quality(80)/shoeboxs/catalog/salgadinho-cheetos-mix-queijo-41g-82118.png`,
+        nome: 'Salgadinho Cheetos snack',
+        preco: '6,24',
+        datavalidade: '08/07/2024',
         desconto: '50%',
         qtdUnit: '25'
     },
 
     {
-        id: 5,
-        fotoproduto: ``,
-        nome: 'Milho Enlatado',
-        preco: '9,90',
-        datavalidade: '12/10/2024',
-        desconto: '50%',
+        id: 15,
+        fotoproduto: `https://images.tcdn.com.br/img/img_prod/645811/salgadinho_fandangos_presunto_37g_elma_chips_3783_1_fa47504ce0aaa7ce7f9d719a75ca9d9f.jpg`,
+        nome: 'Salgadinho Fandangos snack',
+        preco: '6,24',
+        datavalidade: '09/09/2024',
+        desconto: '20%',
+        qtdUnit: '30'
+    },
+
+    {
+        id: 16,
+        fotoproduto: `https://docemalu.vtexassets.com/arquivos/ids/5337590/138458-1.jpg?v=638421661478470000`,
+        nome: 'Salgadinho Fandangos snack',
+        preco: '6,24',
+        datavalidade: '25/08/2024',
+        desconto: '20%',
+        qtdUnit: '24'
+    },
+
+    {
+        id: 17,
+        fotoproduto: `https://http2.mlstatic.com/D_NQ_NP_989340-MLU74842899008_032024-O.webp`,
+        nome: 'Salgadinho Doritos snack',
+        preco: '8,04',
+        datavalidade: '11/07/2024',
+        desconto: '30%',
+        qtdUnit: '17'
+    },
+
+    {
+        id: 18,
+        fotoproduto: `https://img.irroba.com.br/filters:fill(transparent):quality(80)/shoeboxs/catalog/salgadinho-doritos-sweet-chili-53g-81958.png`,
+        nome: 'Salgadinho Doritos snack',
+        preco: '8,60',
+        datavalidade: '15/12/2024',
+        desconto: '15%',
         qtdUnit: '25'
+    },
+
+    //---------- Produtos Massa ---------//
+    {
+        id: 19,
+        fotoproduto: `https://images-food.ifcshop.com.br/produto/21329_0_20200423185609.jpg`,
+        nome: 'Macarrão Dona Benta massa',
+        preco: '3,19',
+        datavalidade: '21/07/2024',
+        desconto: '20%',
+        qtdUnit: '10'
+    },
+
+    {
+        id: 20,
+        fotoproduto: `https://mercantilnovaera.vtexassets.com/arquivos/ids/180884/Macarrao-DONA-BENTA-Penne-Semola-Pacote-500g.jpg?v=637593783312930000`,
+        nome: 'Macarrão Dona Benta massa',
+        preco: '3,19',
+        datavalidade: '12/07/2024',
+        desconto: '20%',
+        qtdUnit: '15'
+    },
+
+    {
+        id: 21,
+        fotoproduto: `https://atacadaobr.vtexassets.com/arquivos/ids/268158/g.jpg?v=638379660435300000`,
+        nome: 'Macarrão Dona Benta massa',
+        preco: '3,19',
+        datavalidade: '12/10/2024',
+        desconto: '20%',
+        qtdUnit: '05'
+    },
+
+    {
+        id: 22,
+        fotoproduto: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhMjojlO38OBzRwYi3q4wM71IN3dS0nYCS_A&s`,
+        nome: 'Macarrão Adria  massa',
+        preco: '2,79',
+        datavalidade: '04/08/2024',
+        desconto: '30%',
+        qtdUnit: '23'
+    },
+
+    {
+        id: 23,
+        fotoproduto: `https://adria.b-cdn.net/wp-content/uploads/2020/11/7896205788132-440x440.png`,
+        nome: 'Macarrão Adria  massa',
+        preco: '2,79',
+        datavalidade: '22/11/2024',
+        desconto: '30%',
+        qtdUnit: '22'
+    },
+
+    {
+        id: 24,
+        fotoproduto: `https://www.clubeextra.com.br/img/uploads/1/722/12707722.jpg`,
+        nome: 'Macarrão Adria  massa',
+        preco: '2,99',
+        datavalidade: '12/11/2024',
+        desconto: '25%',
+        qtdUnit: '25'
+    },
+
+    //-------- Produtos Enlatados ---------//
+    {
+        id: 25,
+        fotoproduto: `https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/33256/medium/milho-verde-quero-lata-170g_90062.png`,
+        nome: 'Milho Verde        Enlatado',
+        preco: '4,72',
+        datavalidade: '05/10/2024',
+        desconto: '25%',
+        qtdUnit: '31'
+    },
+
+    {
+        id: 26,
+        fotoproduto: `https://arguto.com.br/content/produto/010101/016955/T_01052020_090924_822.jpg`,
+        nome: 'Ervilha e         Enlatado',
+        preco: '3,14',
+        datavalidade: '17/08/2024',
+        desconto: '25%',
+        qtdUnit: '35'
+    },
+
+    {
+        id: 27,
+        fotoproduto: `https://lojacentraldealimentos.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/4/9/49117.png`,
+        nome: 'Ervilha        Enlatado',
+        preco: '1,61',
+        datavalidade: '10/07/2024',
+        desconto: '40%',
+        qtdUnit: '25'
+    },
+
+    {
+        id: 28,
+        fotoproduto: `https://www.ebadistribuidora.com.br/889-large_default/pessego-em-calda-metades-lata-ole-450g.webp`,
+        nome: 'Pêssego Olé        Enlatado',
+        preco: '9,34',
+        datavalidade: '03/08/2024',
+        desconto: '50%',
+        qtdUnit: '15'
+    },
+
+    {
+        id: 29,
+        fotoproduto: `https://gomesdacosta.com.br/wp-content/uploads/2017/07/produto-sardinhas-com-oleo-125g.png`,
+        nome: 'Sardinha        Enlatado',
+        preco: '3,57',
+        datavalidade: '01/10/2024',
+        desconto: '35%',
+        qtdUnit: '22'
+    },
+
+    {
+        id: 30,
+        fotoproduto: `https://static.clubeextra.com.br/img/uploads/1/671/18846671.png`,
+        nome: 'Sardinha         Enlatado',
+        preco: '4,63',
+        datavalidade: '27/09/2024',
+        desconto: '20%',
+        qtdUnit: '09'
     },
 
     
