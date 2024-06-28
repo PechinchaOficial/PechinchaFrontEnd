b3e7c2ed22527dd69d2485f8b21a80473c0c56be
Ka1quee <kaiqueoliveira375@gmail.com>
2024-06-28 12:07:35 -0300

    Adding loading in cards

--- src/ConsumerPages/ListagemProdutos/CategoriaProduto/CategoriaProduto.jsx
+++ src/ConsumerPages/ListagemProdutos/CategoriaProduto/CategoriaProduto.jsx
@@ -1,12 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import CardProduto from '../../../components/CardProduto/CardProduto';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import styles from './CategoriaProduto.module.css';
-import { Link } from 'react-router-dom'; // Importe o Link do react-router-dom
+import { Link } from 'react-router-dom';
 import { GetProduto } from '../../../services/ProdutoService';
-import localData from '../../../assets/data/localData'
+import localData from '../../../assets/data/localData';
+
 function Graos({ categoria, category }) {
     const carousel = useRef(null);
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetchProducts();
@@ -15,68 +19,136 @@ function Graos({ categoria, category }) {
     const fetchProducts = async () => {
         try {
             const response = await GetProduto();
-            setProducts(response.data.content);
+            setTimeout(()=>{
+                setProducts(response.data.content);
+                setIsLoading(false);
+            }, 1000)
+  
         } catch (error) {
             console.error("Erro ao buscar produtos", error);
-            // Carregando dados localmente
             setTimeout(() => {
                 setProducts(localData);
-            }, 0);
-            console.error('Erro ao buscar produtos localmente:', error);
-        
-    }
-};
+                setIsLoading(false);
+            }, 1000);
+        }
+    };
 
-const handleLeftClick = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft -= carousel.current.offsetWidth;
-};
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    };
 
-const handleRightClick = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft += carousel.current.offsetWidth;
-};
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    };
 
-// Filtrar os produtos que contêm a palavra "Leite" no nome
-const filteredProducts = products.filter((produto) =>
-    produto.nome.toLowerCase().includes(category ? category.toLowerCase() : '')
-);
+    const filteredProducts = products.filter((produto) =>
+        produto.nome.toLowerCase().includes(category ? category.toLowerCase() : '')
+    );
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "auto"
+        });
+    };
 
+    const truncateString = (str, maxLength) => {
+        if (str.length > maxLength) {
+            return str.substring(0, maxLength) + '...';
+        } else {
+            return str;
+        }
+    };
 
-return (
-    <section className={styles.categoria_produto} id='container'>
-        <div className={styles.title}>
-            <div className={styles.categoria}>
-                <i className="fa-solid fa-store"></i>
-                <h2>{categoria}</h2>
+    return (
+        <section className={styles.categoria_produto} id='container'>
+            <div className={styles.title}>
+                <div className={styles.categoria}>
+                    <i className="fa-solid fa-store"></i>
+                    <h2>{categoria}</h2>
+                </div>
+
+                <Link to='/filterproducts'>
+                    <button className={styles.btn_show_more}>
+                        MOSTRAR MAIS
+                    </button>
+                </Link>
             </div>
 
-            <Link to='/filterproducts'>
-                <button className={styles.btn_show_more}>
-                    MOSTRAR MAIS
-                </button>
-            </Link>
-        </div>
+            <div className={styles.carousel_prod} ref={carousel}>
+                {isLoading ? (
+                    Array.from({ length: 5 }).map((_, index) => (
+                        <div key={index} className={styles.cardproduto}>
+                            <div className={styles.container_img}>
+                                <Skeleton height={200} width={200} />
+                            </div>
+                            <h2 className={styles.prodNome}>
+                                <Skeleton width={150} />
+                            </h2>
+                            <div className={styles.prod_info}>
+                                <span className={styles.info_prod}>
+                                    <Skeleton width={100} />
+                                </span>
+                                <span className={styles.info_prod}>
+                                    <Skeleton width={100} />
+                                </span>
+                            </div>
+                            <div className={styles.valores}>
+                                <span className={styles.preco}>
+                                    <Skeleton width={50} />
+                                </span>
+                                <div className={styles.desconto}>
+                                    <Skeleton width={70} height={70} />
+                                </div>
+                            </div>
+                            <button className={styles.btn}>
+                                <Skeleton width={270}  height={50}/>
+                            </button>
+                        </div>
+                    ))
+                ) : (
+                    filteredProducts.map(({ id, nome, datavalidade, preco, desconto, fotoproduto }) => (
+                        <Link key={id} to={`/produto/${id}`} className={styles.productLink}>
+                            <div className={styles.cardproduto}>
+                                <div className={styles.container_img} onClick={scrollToTop}>
+                                    <img src={fotoproduto} alt="Produto" className={styles.imgprod} />
+                                </div>
+                                <h2 className={styles.prodNome}>{truncateString(nome, 15)}</h2>
+                                <div className={styles.prod_info}>
+                                    <span className={styles.info_prod}>
+                                        Data: até {datavalidade}
+                                    </span>
+                                    <span className={styles.info_prod}>
+                                        10 Unidades restantes
+                                    </span>
+                                </div>
+                                <div className={styles.valores}>
+                                    <span className={styles.preco}>
+                                        R$ {preco}
+                                    </span>
+                                    <div className={styles.desconto}>
+                                        <span className={styles.porcentagem}>
+                                            -{desconto}
+                                        </span>
+                                    </div>
+                                </div>
+                                <Link to='/cart'>
+                                    <button className={styles.btn} onClick={scrollToTop}>
+                                        ADICIONAR
+                                    </button>
+                                </Link>
+                            </div>
+                        </Link>
+                    ))
+                )}
+            </div>
 
-        <div className={styles.carousel_prod} ref={carousel}>
-            {filteredProducts.map(({ id, nome, datavalidade, preco, desconto, fotoproduto }) => (
-                <Link key={id} to={`/produto/${id}`} className={styles.productLink}>
-                    <CardProduto
-                        name={nome}
-                        dateVenc={datavalidade}
-                        price={preco}
-                        discount={desconto}
-                        img={fotoproduto}
-                    />
-                </Link>
-            ))}
-        </div>
-
-        <button className={styles.btn_carousel} onClick={handleLeftClick} id={styles.left}><i className="fa-solid fa-chevron-left"></i></button>
-        <button className={styles.btn_carousel} onClick={handleRightClick} id={styles.right}><i className="fa-solid fa-chevron-right"></i></button>
-    </section>
-);
+            <button className={styles.btn_carousel} onClick={handleLeftClick} id={styles.left}><i className="fa-solid fa-chevron-left"></i></button>
+            <button className={styles.btn_carousel} onClick={handleRightClick} id={styles.right}><i className="fa-solid fa-chevron-right"></i></button>
+        </section>
+    );
 }
 
 export default Graos;
--- src/ConsumerPages/Product/Product.jsx
+++ src/ConsumerPages/Product/Product.jsx
@@ -3,31 +3,34 @@ import HeaderConsumer from '../../components/HeaderConsumer/HeaderConsumer';
 import CategoriaProduto from '../ListagemProdutos/CategoriaProduto/CategoriaProduto';
 import Styles from './Product.module.css';
 import { useParams } from 'react-router-dom';
-import { GetProdutoById } from '../../services/ProdutoService'; // Supondo que você tenha um serviço para obter detalhes do produto
+import { GetProdutoById } from '../../services/ProdutoService';
 import localData from '../../assets/data/localData';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function Product() {
-    const { id } = useParams();
-    const [produto, setProduto] = useState(null);
+    const { id } = useParams(); 
+    const [produto, setProduto] = useState(null); 
 
     useEffect(() => {
-        fetchProduto();
-    }, []);
+        fetchProduto(); 
+    }, [id]);
 
     const fetchProduto = async () => {
         try {
-            const response = await GetProdutoById(id); // Função para buscar produto por id
-            console.log(response);
-            setProduto(response.data); // Define os detalhes do produto no estado local
+            console.log("Fetching product with ID:", id);
+            const response = await GetProdutoById(id);
+            console.log("API response:", response.data);
+           setTimeout(()=>{
+            setProduto(response.data); 
+           }, 1000)
         } catch (error) {
             console.error("Erro ao buscar produto", error);
-            setProduto(localData); // Carrega dados locais em caso de erro
+            const localProduto = localData.find(produto => String(produto.id) === String(id));
+            console.log("Local product found:", localProduto);
+            setProduto(localProduto);
         }
     };
-
-    if (!produto) {
-        return <div>Carregando...</div>; // Exibe uma mensagem de carregamento enquanto os dados do produto estão sendo carregados
-    }
 
     return (
         <div>
@@ -44,12 +47,20 @@ function Product() {
                     <div className={Styles.BackProducts}>
                         <article className={Styles.BoxLeft}>
                             <div className={Styles.Box1}>
-                                <img src={produto.fotoproduto} alt="imagem do produto" className={Styles.img_product_2} />
+                                {produto ? (
+                                    <img src={produto.fotoproduto} alt="imagem do produto" className={Styles.img_product_2} />
+                                ) : (
+                                    <Skeleton height={100} width={100} />
+                                )}
                             </div>
                         </article>
                         <article className={Styles.BoxRight}>
                             <div className={Styles.Box2}>
-                                <img src={produto.fotoproduto} alt="imagem do produto" className={Styles.img_product} />
+                                {produto ? (
+                                    <img src={produto.fotoproduto} alt="imagem do produto" className={Styles.img_product} />
+                                ) : (
+                                    <Skeleton height={212} width={212} />
+                                )}
                             </div>
                         </article>
                     </div>
@@ -57,17 +68,39 @@ function Product() {
                         <article className={Styles.BoxInfo}>
                             <div className={Styles.InfoLeft}>
                                 <div className={Styles.BoxInfoProduct}>
-                                    <h4 className={Styles.titulo_produto}>{produto.nome}</h4>
-                                    <h5 className={Styles.sub_titulo_product}>{produto.descricao}</h5>
+                                    {produto ? (
+                                        <>
+                                            <h4 className={Styles.titulo_produto}>{produto.nome}</h4>
+                                            <h5 className={Styles.sub_titulo_product}>{produto.descricao}</h5>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Skeleton height={30} width={200} />
+                                            <Skeleton height={20} width={300} />
+                                        </>
+                                    )}
                                 </div>
                                 <div className={Styles.DateValue}>
                                     <div className={Styles.BoxdateValue}>
-                                        <h4 className={Styles.Date}> Vence: {produto.datavalidade} </h4>
-                                        <h2 className={Styles.Value}>R$ {produto.preco} </h2>
+                                        {produto ? (
+                                            <>
+                                                <h4 className={Styles.Date}> Vence: {produto.datavalidade} </h4>
+                                                <h2 className={Styles.Value}>R$ {produto.preco} </h2>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <Skeleton height={20} width={150} />
+                                                <Skeleton height={30} width={100} />
+                                            </>
+                                        )}
                                     </div>
                                     <div className={Styles.BoxDesc}>
                                         <div className={Styles.Desc}>
-                                            <h1>{produto.desconto}%</h1>
+                                            {produto ? (
+                                                <h1>-{produto.desconto}%</h1>
+                                            ) : (
+                                                <Skeleton height={70} width={80} />
+                                            )}
                                         </div>
                                     </div>
                                 </div>
@@ -82,8 +115,20 @@ function Product() {
                                     <div className={Styles.Values}>
                                         <input type="number" placeholder='1' className={Styles.QtdBtn} />
                                     </div>
-                                    <div className={Styles.Values}><p> R$ {(produto.preco / 100).toFixed(2)} </p></div>
-                                    <div className={Styles.Values}><p> R$ {(produto.preco / 100).toFixed(2)} </p></div>
+                                    <div className={Styles.Values}>
+                                        {produto ? (
+                                            <p> R$ {(produto.preco / 100).toFixed(2)} </p>
+                                        ) : (
+                                            <Skeleton height={20} width={50} />
+                                        )}
+                                    </div>
+                                    <div className={Styles.Values}>
+                                        {produto ? (
+                                            <p> R$ {(produto.preco / 100).toFixed(2)} </p>
+                                        ) : (
+                                            <Skeleton height={20} width={50} />
+                                        )}
+                                    </div>
                                 </div>
                                 <button className={Styles.Btn}>
                                     <p>Finalizar compra</p>
