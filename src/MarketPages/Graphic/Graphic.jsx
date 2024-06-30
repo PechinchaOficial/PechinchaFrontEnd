8b100cf781e00770e4928fc374d1e6abbca0c760
CesarCouto <cesarrcouto@outlook.com>
2024-06-30 14:31:27 -0300

    finished data analysis page

--- src/MarketPages/Graphic/Graphic.jsx
+++ src/MarketPages/Graphic/Graphic.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Line, Pie } from 'react-chartjs-2';
+import { Line, Pie, Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
 import styles from './Graphic.module.css'; // Import CSS Module
 import HeaderMarket from '../../components/CardProduct/HeaderMarket/HeaderMarket';
 import { Link } from 'react-router-dom';
-import Add from '../../assets/img/Add.png'
-import Home from '../../assets/img/Home_markt.png'
-import Produtos from '../../assets/img/Produtos_market.png'
-import Add_Market from '../../assets/img/Add_market.png'
-import Conta from '../../assets/img/Conta.png'
-import Sair from '../../assets/img/Sair.png'
+import Add from '../../assets/img/Add.png';
+import Home from '../../assets/img/Home_markt.png';
+import Produtos from '../../assets/img/Produtos_market.png';
+import Add_Market from '../../assets/img/Add_market.png';
+import Conta from '../../assets/img/Conta.png';
+import Sair from '../../assets/img/Sair.png';
 
 const Graphic = () => {
   const lineData = {
@@ -39,8 +39,8 @@ const Graphic = () => {
         display: true,
         text: 'Dias com mais vendas',
         font: {
-          size: 24
-        }
+          size: 24,
+        },
       },
       legend: {
         display: true,
@@ -48,12 +48,12 @@ const Graphic = () => {
         labels: {
           boxWidth: 20,
           padding: 15,
-        }
+        },
       },
       subtitle: {
         display: true,
         text: 'Gráfico feito para visualizar os dias com mais vendas do seu mercado',
-      }
+      },
     },
     scales: {
       y: {
@@ -64,7 +64,7 @@ const Graphic = () => {
   };
 
   const pieData = {
-    labels: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'],
+    labels: ['Chocolate', 'Salgadinho', 'Arroz', 'Snack', 'Massa', 'Bebida', 'Outros'],
     datasets: [
       {
         label: 'Vendas',
@@ -76,7 +76,7 @@ const Graphic = () => {
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
           'rgba(255, 159, 64, 0.2)',
-          'rgba(99, 255, 132, 0.2)'
+          'rgba(99, 255, 132, 0.2)',
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
@@ -85,10 +85,10 @@ const Graphic = () => {
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
           'rgba(255, 159, 64, 1)',
-          'rgba(99, 255, 132, 1)'
+          'rgba(99, 255, 132, 1)',
         ],
         borderWidth: 1,
-      }
+      },
     ],
   };
 
@@ -97,10 +97,10 @@ const Graphic = () => {
     plugins: {
       title: {
         display: true,
-        text: 'Distribuição de Vendas por Dia da Semana',
+        text: 'Produtos mais vendidos por dia da semana',
         font: {
-          size: 24
-        }
+          size: 24,
+        },
       },
       legend: {
         display: true,
@@ -108,12 +108,51 @@ const Graphic = () => {
         labels: {
           boxWidth: 20,
           padding: 15,
-        }
+        },
       },
       subtitle: {
         display: true,
-        text: 'Gráfico de pizza mostrando a distribuição das vendas durante a semana',
-      }
+        text: 'Gráfico de pizza mostrando a distribuição das vendas de produtos referentes ao dia da semana',
+      },
+    },
+  };
+
+  const barData = {
+    labels: ['Semana 1', 'Semana 2', 'Semana 3'],
+    datasets: [
+      {
+        label: 'Quantidade Vendida',
+        data: [50, 75, 100],
+        backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(75, 192, 192, 0.5)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const barOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Vendas apos começar a usar a plataforma',
+        font: {
+          size: 24,
+        },
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          boxWidth: 20,
+          padding: 15,
+        },
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
     },
   };
 
@@ -121,101 +160,124 @@ const Graphic = () => {
     <div>
       <HeaderMarket />
       <div className={styles.container_pai}>
-      <nav className={styles.side_bar}>  {/*Nav-bar lateral */}
-
-<div className={styles.div_mercado}> {/* Div que mostra o nome e a imagem do mercado*/}
-
-    <div className={styles.imagem_mercado}> {/* Imagem do mercado */}
-
-    </div>
-
-    <div className={styles.nome_mercado}> {/* Div para o nome do mercado */}
-        <h2 className={styles.titulo_mercado}>Mercado Todo dia</h2>
-        <p className={styles.nome_responsavel}>Responsavel</p>
-    </div>
-</div>
-
-<div className={styles.container_btn}> {/*Container para o btn*/}
-    <Link to="/productregistration">
-        <button className={styles.btn_cds_produto}> {/*Btn para cadastrar produto*/}
-            <div className={styles.conteudo_btn}> {/*Conteudo do btn*/}
-
-                <div className={styles.container_img_add}> {/*Container imagem add*/}
+        <nav className={styles.side_bar}>
+          {/*Nav-bar lateral */}
+          <div className={styles.div_mercado}>
+            {/* Div que mostra o nome e a imagem do mercado*/}
+            <div className={styles.imagem_mercado}>
+              {/* Imagem do mercado */}
+            </div>
+            <div className={styles.nome_mercado}>
+              {/* Div para o nome do mercado */}
+              <h2 className={styles.titulo_mercado}>Mercado Todo dia</h2>
+              <p className={styles.nome_responsavel}>Responsavel</p>
+            </div>
+          </div>
+          <div className={styles.container_btn}>
+            {/*Container para o btn*/}
+            <Link to="/productregistration">
+              <button className={styles.btn_cds_produto}>
+                {/*Btn para cadastrar produto*/}
+                <div className={styles.conteudo_btn}>
+                  {/*Conteudo do btn*/}
+                  <div className={styles.container_img_add}>
+                    {/*Container imagem add*/}
                     <img src={Add} alt="imagem de um mais" />
-                </div>
-
-
-                <div className={styles.container_texto_cadastro}> {/*Texto cadastro*/}
+                  </div>
+                  <div className={styles.container_texto_cadastro}>
+                    {/*Texto cadastro*/}
                     <p className={styles.texto_cadastro}>Cadastrar Produto</p>
+                  </div>
                 </div>
-
+              </button>
+            </Link>
+          </div>
+          <Link to="/marketSystem">
+            <div className={styles.div_nav}>
+              {/*Div Nav-bar*/}
+              <div className={styles.container_central}>
+                {/*Container centralizado*/}
+                <div className={styles.container_img_nav}>
+                  {/*Container da imagem*/}
+                  <img src={Home} alt="imagem de uma casa" />
+                </div>
+                <div className={styles.container_texto_nav}>
+                  {/*Texto da nav*/}
+                  <p className={styles.texto_nav}>Pagina Inicial</p>
+                </div>
+              </div>
             </div>
-        </button>
-    </Link>
-</div>
-<Link to="/marketSystem">
-<div className={styles.div_nav}>  {/*Div Nav-bar*/}
-    <div className={styles.container_central}> {/*Container centralizado*/}
-        <div className={styles.container_img_nav}> {/*Container da imagem*/}
-            <img src={Home} alt="imagem de uma casa" />
-        </div>
+          </Link>
+          <Link to="/marketproduct">
+            <div className={styles.div_nav}>
+              {/*Div Nav-bar*/}
+              <div className={styles.container_central}>
+                {/*Container centralizado*/}
+                <div className={styles.container_img_nav}>
+                  {/*Container da imagem*/}
+                  <img src={Produtos} alt="imagem de uma casa" />
+                </div>
+                <div className={styles.container_texto_nav}>
+                  {/*Texto da nav*/}
+                  <p className={styles.texto_nav}>Produtos</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <div className={styles.div_nav}>
+            {/*Div Nav-bar*/}
+            <div className={styles.container_central}>
+              {/*Container centralizado*/}
+              <div className={styles.container_img_nav}>
+                {/*Container da imagem*/}
+                <img src={Conta} alt="imagem de uma casa" />
+              </div>
+              <div className={styles.container_texto_nav}>
+                {/*Texto da nav*/}
+                <p className={styles.texto_nav}>Minha conta</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.div_nav}>
+            {/*Div Nav-bar*/}
+            <Link to="/" className={styles.container_central}>
+              <div className={styles.container_central}>
+                {/*Container centralizado*/}
+                <div className={styles.container_img_nav}>
+                  {/*Container da imagem*/}
+                  <img src={Sair} alt="imagem de uma casa" />
+                </div>
+                <div className={styles.container_texto_nav}>
+                  {/*Texto da nav*/}
+                  <p className={styles.texto_nav}>Sair</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </nav>
 
-        <div className={styles.container_texto_nav}> {/*Texto da nav*/}
-            <p className={styles.texto_nav}>Pagina Inicial</p>
-        </div>
-    </div>
-</div>
-</Link>
-<Link to="/marketproduct">
-<div className={styles.div_nav}>  {/*Div Nav-bar*/}
-    <div className={styles.container_central}> {/*Container centralizado*/}
-        <div className={styles.container_img_nav}> {/*Container da imagem*/}
-            <img src={Produtos} alt="imagem de uma casa" />
-        </div>
+       <div className={styles.graphics}>
 
-        <div className={styles.container_texto_nav}> {/*Texto da nav*/}
-            <p className={styles.texto_nav}>Produtos</p>
-        </div>
-    </div>
-</div>
-</Link>
+          <div className={styles.container_graphic}>
 
-
-
-<div className={styles.div_nav}>  {/*Div Nav-bar*/}
-    <div className={styles.container_central}> {/*Container centralizado*/}
-        <div className={styles.container_img_nav}> {/*Container da imagem*/}
-            <img src={Conta} alt="imagem de uma casa" />
-        </div>
-
-        <div className={styles.container_texto_nav}> {/*Texto da nav*/}
-            <p className={styles.texto_nav}>Minha conta</p>
-        </div>
-    </div>
-</div>
-
-<div className={styles.div_nav}>  {/*Div Nav-bar*/}
-    <Link to="/" className={styles.container_central} >
-        <div className={styles.container_central}> {/*Container centralizado*/}
-            <div className={styles.container_img_nav}> {/*Container da imagem*/}
-                <img src={Sair} alt="imagem de uma casa" />
+            <div className={styles.container}>
+              <Line data={lineData} options={lineOptions} />
             </div>
 
-            <div className={styles.container_texto_nav}> {/*Texto da nav*/}
-                <p className={styles.texto_nav}>Sair</p>
+            <div className={styles.container_2}>
+              <Pie data={pieData} options={pieOptions} />
             </div>
-        </div>
-    </Link>
-</div>
 
+          </div>     
 
-</nav>
-         <div className={styles.container}>
-          <Line data={lineData} options={lineOptions} />
-        </div>
-        <div className={styles.container}>
-          <Pie data={pieData} options={pieOptions} />
-        </div> 
+          <div className={styles.container_graphic}>
+
+            <div className={styles.container_3}>
+              <Bar data={barData} options={barOptions} />
+            </div>  
+          </div>
+
+       </div>
       </div>
     </div>
   );
