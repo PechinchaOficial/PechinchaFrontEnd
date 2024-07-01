2b2ee85e6fef65ab5f1bfb607d8cf890f15592dc
GabrielGallo1 <gabriel.gallo194@gmail.com>
2024-07-01 17:03:21 -0300

    Responsive product, Carousel client and website polishing.

--- src/ConsumerPages/Product/Product.jsx
+++ src/ConsumerPages/Product/Product.jsx
@@ -54,7 +54,7 @@ function Product() {
                 <article className={Styles.Boxtext}>
                     <h1 className={Styles.title}>Finalize seu pedido</h1>
                     <div className={Styles.Subtitlebox}>
-                        <div className={Styles.icoHome}> <img src="" alt="icone home" /> </div>
+                        <div className={Styles.icoHome}> <i class="fa-solid fa-house"></i> </div>
                         <h4 className={Styles.Subtitle}>Rua.Tito N450</h4>
                     </div>
                 </article>
