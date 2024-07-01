467141df30557c1769074ff7fb250c84aa6def1c
CesarCouto <cesarrcouto@outlook.com>
2024-07-01 18:29:44 -0300

    linking review

--- src/MarketPages/Home/HeroSection/HeroSection.jsx
+++ src/MarketPages/Home/HeroSection/HeroSection.jsx
@@ -1,5 +1,6 @@
 import Styles from './HeroSection.module.css'
 import Hero from './Hero/Hero';
+import { Link } from 'react-router-dom';
 
 function HeroMarket() {
     return(
@@ -7,15 +8,15 @@ function HeroMarket() {
             <article className={Styles.Hero}>
                 <div className={Styles.Banner}>
                     <Hero />
-                </div>
+                </div> 
             </article>
             <article className={Styles.BodySect}>
                 <div className={Styles.Backtext}>
                 <p>Divulgue os produtos do seu mercado <br/> que não venderam e reduza o prejuizo.</p>
                 </div>
                 <div className={Styles.BackBtns}>
-                    <button className={Styles.Btn} id={Styles.RegisterBtn}><p>CADASTRAR MERCADO</p></button>
-                    <button className={Styles.Btn} id={Styles.KnowMoreBtn}><p>SAIBA MAIS</p></button>
+                    <button className={Styles.Btn} id={Styles.RegisterBtn}><Link  className={Styles.link} to="/registerMarket"><p>CADASTRAR MERCADO</p></Link></button>
+                <button className={Styles.Btn} id={Styles.KnowMoreBtn}><p>SAIBA MAIS</p></button>
                 </div>
             </article>
         </section>
